ee6e992cf295d4427b8f2542d6ae068dbc023d2f
Archi <alejandro_19873000@hotmail.com>
2020-07-27 01:20:23 -0300

    Merge origin/master

--- resources/views/registrarse.blade.php
+++ resources/views/registrarse.blade.php
@@ -2,46 +2,47 @@
 
 @section('seccion')
 
-<form action="{{ route('usuario.registrarse')}}" method="POST">
+<form action="{{ route('usuario.registrarse')}}" method="post">
     @csrf
     <div class="row">
       <div class="col">
-      <label for="exampleInputPassword1">Usuario</label>
-      <input type="text" class="form-control" id="exampleInputPassword1" required>
+      <label for="user">Usuario</label>
+      <input type="text" class="form-control" id="inputUser" name="user" required>
       <div class="invalid-feedback">Debe ingresar un usuario</div>
       </div>
       <div class="col">
-        <label for="exampleInputPassword1">Contraseña</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" required>
+        <label for="password">Contraseña</label>
+        <input type="password" class="form-control" id="inputPassword" name="password" required>
         <div class="invalid-feedback">Debe ingresar un usuario</div>
       </div>
     </div>
     <div class="row">
       <div class="col">
-        <label for="exampleInputPassword1">Nombre</label>
-        <input type="text" class="form-control" required>
+        <label for="name">Nombre</label>
+        <input type="text" class="form-control" name="name" required>
         <div class="invalid-feedback">Debe ingresar su nombre</div> 
       </div>
       <div class="col">
-        <label for="exampleInputPassword1">Apellido</label>
-        <input type="text" class="form-control" required>
+        <label for="lastName">Apellido</label>
+        <input type="text" class="form-control" name="lastname" required>
         <div class="invalid-feedback">Debe ingresar su apellido</div> 
       </div>
     </div>
     <div class="row">
       <div class="col">
-        <label for="exampleInputPassword1">Fecha de Nacimiento</label>
-        <input type="text" class="form-control" required>
+        <label for="birthday">Fecha de Nacimiento</label>
+        <input type="text" class="form-control" name="birthday" required>
       </div>
       <div class="col">
-        <label for="exampleInputPassword1">Teléfono</label>
-        <input type="text" class="form-control">
+        <label for="phone">Teléfono</label>
+        <input type="text" class="form-control" name="phone">
       </div>
     </div>
   <div class="form-group">
     <label for="exampleInputEmail1">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="mail" required>
   </div>
   <button type="submit" class="btn btn-primary">Registrarse</button>
 </form>
+
 @endsection
